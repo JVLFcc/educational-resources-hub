7b248fe8982ecd61a3027e8661344403c3e108b2
jvlf <jvlf@cin.ufpe.br>
2026-03-03 00:01:20 -0300

    refactor(schemas): update resource schemas and enforce resource_type enum

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,10 +1,18 @@
 from pydantic import BaseModel
+from typing import List, Optional
+from enum import Enum
 
 #? isso vai ser usado para criar os schemas que vão validar os dados de i/o da API, e pra diferir a estrutura da tabela da estrutura da API para que não haja vazamento de informações da tabela para a API, e pra que seja possível da API ter uma estrutura diferente da tabela
+class ResourceType(str, Enum):
+    video = "Video"
+    pdf = "PDF"
+    link = "link"
 class ResourceBase(BaseModel):
     title: str
-    description: str
-    resource_type: str
+    description: Optional[str] = None
+    resource_type: ResourceType
+    url: Optional[str] = None
+    tags: Optional[List[str]] = None
 
 class ResourceCreate(ResourceBase):
     pass
@@ -14,3 +22,11 @@ class ResourceResponse(ResourceBase):
     
     class Config:
         from_attributes = True
+
+class SmartAssistRequest(BaseModel):
+    title: str
+    resource_type: ResourceType
+
+class SmartAssistResponse(BaseModel):
+    description: str
+    tags: List[str]
