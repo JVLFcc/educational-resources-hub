2a9979ebf50670d7e710b031709b6cc28a925033
jvlf <jvlf@cin.ufpe.br>
2026-03-03 00:05:33 -0300

    feat: configure basic logging format and level

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 from .db import engine
 from . import models
 from .routers import resources
+import logging
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format="%(asctime)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+    )
 
 models.Base.metadata.create_all(bind=engine)
 
